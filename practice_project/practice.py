55ca1f95101a065dcfa40891bc0e7e5cf52d15f7
AdelineO1 <113532900+AdelineO1@users.noreply.github.com>
2024-03-04 04:26:47 +0000

    Modified is_prime function and test is 5 and 8 are prime numbers

--- practice_project/practice.py
+++ practice_project/practice.py
@@ -1,10 +1,10 @@
 # Test whether numbers in a given range is prime
 def is_prime(num):
-    for n in range (0,9):
+    for n in range (2, int(num**0.5) + 1):
         if num%n==0:
             return False
         return True
 
 if __name__=='main':
-    print (is_prime(7)) # True
-    print (is_prime(4)) # False
+    print (is_prime(5)) # True
+    print (is_prime(8)) # False
